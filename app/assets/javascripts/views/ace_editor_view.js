2c9d4f3edbe7e5bd6e7d503ee30e75c68d49241c
hi104 <shohiti@gmail.com>
2014-01-05 13:17:51 +0900

    add sync scroll

--- app/assets/javascripts/views/ace_editor_view.js
+++ app/assets/javascripts/views/ace_editor_view.js
@@ -1,59 +1,77 @@
+
 /*global ace:true */
 
-/* copy from Discourse AceEditorView thank you !*/
+/**
+ copy from Discourse AceEditorView thank you
+ https://github.com/discourse/discourse
+ **/
 
 /**
-  A view that wraps the ACE editor (http://ace.ajax.org/)
-  @class AceEditorView
-**/
+ A view that wraps the ACE editor (http://ace.ajax.org/)
+ @class AceEditorView
+ **/
+
 
 App.AceEditorView = Ember.View.extend({
-  // mode: 'css',
-  mode: 'markdown',
-  classNames: ['ace-wrapper'],
+    // mode: 'css',
+    mode: 'markdown',
+    classNames: ['ace-wrapper'],
 
-  contentChanged: (function() {
-    if (this.editor && !this.skipContentChangeEvent) {
-      return this.editor.getSession().setValue(this.get('content'));
+    contentChanged: (function() {
+        if (this.editor && !this.skipContentChangeEvent) {
+            return this.editor.getSession().setValue(this.get('content'));
+        }
+    }).observes('content'),
+
+    render: function(buffer) {
+        buffer.push("<div class='ace'>");
+        if (this.get('content')) {
+            buffer.push(Handlebars.Utils.escapeExpression(this.get('content')));
+        }
+        return buffer.push("</div>");
+    },
+
+    willDestroyElement: function() {
+        if (this.editor) {
+            this.editor.destroy();
+            this.editor = null;
+        }
+    },
+    syncScroll: function () {
+        var editor = this.editor;
+        var previewContent = $('#preview-content');
+
+        var editorLength = editor.getSession().getLength();
+        var editorHeight =  $('.ace').height();
+        var previewHeight = previewContent.height();
+        var ratio = editor.getFirstVisibleRow() / editorLength;
+        $('#preview').scrollTop(ratio * previewHeight);
+
+    },
+
+    didInsertElement: function() {
+
+        var aceEditorView = this;
+
+        var initAce = function() {
+            aceEditorView.editor = ace.edit(aceEditorView.$('.ace')[0]);
+            aceEditorView.editor.setTheme("ace/theme/chrome");
+            aceEditorView.editor.setShowPrintMargin(false);
+            // aceEditorView.editor.renderer.setShowGutter(false); 
+            aceEditorView.editor.getSession().setUseWrapMode(true);
+            aceEditorView.editor.getSession().setMode("ace/mode/" + (aceEditorView.get('mode')));
+            aceEditorView.editor.on("change", function(e) {
+                aceEditorView.skipContentChangeEvent = true;
+                aceEditorView.set('content', aceEditorView.editor.getSession().getValue());
+                aceEditorView.skipContentChangeEvent = false;
+            });
+
+            aceEditorView.editor.getSession().on('changeScrollTop', aceEditorView.syncScroll.bind(aceEditorView));
+        };
+
+        if (window.ace) {
+            initAce();
+        }
     }
-  }).observes('content'),
-
-  render: function(buffer) {
-    buffer.push("<div class='ace'>");
-    if (this.get('content')) {
-      buffer.push(Handlebars.Utils.escapeExpression(this.get('content')));
-    }
-    return buffer.push("</div>");
-  },
-
-  willDestroyElement: function() {
-    if (this.editor) {
-      this.editor.destroy();
-      this.editor = null;
-    }
-  },
-
-  didInsertElement: function() {
-
-    var aceEditorView = this;
-
-    var initAce = function() {
-      aceEditorView.editor = ace.edit(aceEditorView.$('.ace')[0]);
-      aceEditorView.editor.setTheme("ace/theme/chrome");
-      aceEditorView.editor.setShowPrintMargin(false);
-      // aceEditorView.editor.renderer.setShowGutter(false); 
-      aceEditorView.editor.getSession().setUseWrapMode(true);
-      aceEditorView.editor.getSession().setMode("ace/mode/" + (aceEditorView.get('mode')));
-      aceEditorView.editor.on("change", function(e) {
-        aceEditorView.skipContentChangeEvent = true;
-        aceEditorView.set('content', aceEditorView.editor.getSession().getValue());
-        aceEditorView.skipContentChangeEvent = false;
-      });
-    };
-
-    if (window.ace) {
-      initAce();
-    }
-  }
 });
 
