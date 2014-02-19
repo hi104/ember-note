8b61fb641d6efe4d87c1c0f6530af92ddb8e71a1
hi104 <shohiti@gmail.com>
2014-02-19 22:11:46 +0900

    delete none use code

--- app/controllers/note_attachments_controller.rb
+++ app/controllers/note_attachments_controller.rb
@@ -1,30 +1,8 @@
 class NoteAttachmentsController < ApplicationController
   layout "login"
-  before_action :set_note_attachment, only: [:show, :edit, :update, :destroy]
+  before_action :set_note_attachment, only: [:destroy]
   before_action :set_note
 
-  # GET /note_attachments
-  # GET /note_attachments.json
-  def index
-    @note_attachments = NoteAttachment.all
-  end
-
-  # GET /note_attachments/1
-  # GET /note_attachments/1.json
-  def show
-  end
-
-  # GET /note_attachments/new
-  def new
-    @note_attachment = NoteAttachment.new
-  end
-
-  # GET /note_attachments/1/edit
-  def edit
-  end
-
-  # POST /note_attachments
-  # POST /note_attachments.json
   def create
     @note_attachment = NoteAttachment.new(note_attachment_params)
     @note_attachment.note = @note
@@ -34,20 +12,6 @@ class NoteAttachmentsController < ApplicationController
         format.json { render action: 'show' }
       else
         format.html { render action: 'new' }
-        format.json { render json: @note_attachment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /note_attachments/1
-  # PATCH/PUT /note_attachments/1.json
-  def update
-    respond_to do |format|
-      if @note_attachment.update(note_attachment_params)
-        format.html { redirect_to @note_attachment, notice: 'Note attachment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
         format.json { render json: @note_attachment.errors, status: :unprocessable_entity }
       end
     end
